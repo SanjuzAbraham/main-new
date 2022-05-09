ceb7ff288afdeaaadfd340ed6fc7c42d88a3d609
Sanju <sanjuabraham321@gmail.com>
2022-05-09 08:56:34 +0530

    First

---
+++ middlewares/cloudinary.js
@@ -0,0 +1,10 @@
+var cloudinary = require('cloudinary').v2
+
+
+cloudinary.config({
+    cloud_name:"sanjuabraham",
+    api_key:"393847892775719",
+    api_secret: "2GgTg4WV2Hvqdxflr3K-s69jYfg",
+})
+
+module.exports=cloudinary
